3f3a2eaf47288929e8ed472fc4577aa89007279b
Jamshed Ahmed <jamsheda4ahmed786@gmail.com>
2022-04-04 19:30:42 +0500

    Created Listing Table Component
    - Created Listing Table Component
    - Updated Doctor, Staff, Admin, Accountant User listing table components.

--- src/sections/AccountantUser/AccountantListingTable.tsx
+++ src/sections/AccountantUser/AccountantListingTable.tsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
-import { AgGridReact } from "ag-grid-react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import { DeleteRounded, EditRounded, PrintRounded } from "@mui/icons-material";
+import { DeleteRounded, EditRounded } from "@mui/icons-material";
 // ADD DATA/UTILS import below this comment
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import { IAccountant } from "../../utils/interfaces/accountant.interface";
-import { GridApi, GridReadyEvent, ICellRendererParams } from "ag-grid-community";
-import { PRINT } from "../../utils/langauge/en/buttonLabels";
+import { ICellRendererParams } from "ag-grid-community";
+import ListingTable from "../../components/ListingTable";
 
 const ActionCellRenderer: React.FC<ICellRendererParams> = (props) => {
   const cellValue = props.data;
@@ -30,28 +27,9 @@ interface IAccountantsTableProps {
   rowData: IAccountant[];
 }
 
-const AccountantsListingTable: React.FC<IAccountantsTableProps> = ({ rowData }) => {
-
-  const [gridApi, setGridApi] =useState<GridApi>();
-
-  function onGridReady(params: GridReadyEvent) {
-    setGridApi(params.api)
-    params.api.sizeColumnsToFit();
-  }
-
-  //* use this function to achieve bulk actions like delete, print etc.
-  const getSelectedRowData = () => {
-    let selectedNodes = gridApi?.getSelectedNodes();
-    let selectedData = selectedNodes?.map(node => node.data);
-    console.log(`Selected Nodes:\n${JSON.stringify(selectedData)}`);
-    return selectedData;
-  };
-
-  const defaultColDef = {
-    resizable: true,
-    filter: true,
-    sortable: true,
-  };
+const AccountantsListingTable: React.FC<IAccountantsTableProps> = ({
+  rowData,
+}) => {
   //* ADD TABLE CELLS HERE
   const [columnDefs] = useState([
     { field: "name", headerCheckboxSelection: true, checkboxSelection: true },
@@ -64,30 +42,7 @@ const AccountantsListingTable: React.FC<IAccountantsTableProps> = ({ rowData }) 
       cellRenderer: ActionCellRenderer,
     },
   ]);
-  return (
-    <>
-      <Box sx={{ margin: "1rem", display: "flex", justifyContent: "flex-end" }}>
-        <Button endIcon={<PrintRounded />} onClick={getSelectedRowData}>
-          {PRINT}
-        </Button>
-      </Box>
-      <div
-        className="ag-theme-alpine"
-        style={{ height: "500px", width: "auto", marginTop: "20px" }}
-      >
-        <AgGridReact
-          onGridReady={onGridReady}
-          rowData={rowData}
-          columnDefs={columnDefs}
-          defaultColDef={defaultColDef}
-          pagination={true}
-          paginationPageSize={20}
-          rowSelection={"multiple"}
-          suppressRowClickSelection={true}
-        />
-      </div>
-    </>
-  );
+  return <ListingTable rowData={rowData} columnDefs={columnDefs} />;
 };
 
 export default AccountantsListingTable;
