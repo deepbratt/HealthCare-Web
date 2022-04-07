490e19d5ffc27f77cb1bad435bc49dec7f73d3ad
basuabhirup <basu.abhirup@yahoo.com>
2022-04-07 21:28:25 +0530

    Update sidebar

--- src/pages/patientDetails/index.tsx
+++ src/pages/patientDetails/index.tsx
@@ -1,17 +1,94 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Card, CardContent, CardHeader, Grid, Paper, Typography } from "@mui/material";
+import { Avatar, Button, Card, CardContent, CardHeader, Grid, IconButton, Paper, Stack, Typography } from "@mui/material";
+import { ChevronRight, MoreVert, PermIdentity, PhoneAndroid, Tag } from "@mui/icons-material";
+import { blue } from "@mui/material/colors";
+import { styled } from "@mui/system";
 
 const PatientDetails: React.FC = () => {
   const { id } = useParams();
+
+  const Item = styled(Paper)(({ theme }) => ({
+    borderTop: `1px solid #CCC`,
+    borderRadius: 0,
+    padding: theme.spacing(1),
+    textAlign: 'left',
+    display: 'flex',
+    alignItems: 'center',
+  }));
 
   // Get the above particular patients's details from API Call
 
   return (
     <Grid container spacing={1} sx={{ padding: '0.5rem' }}>
       <Grid item xs={2}>
-        <Grid item xs={12} sx={{ border: '1px solid blue', height: '21rem', marginBottom: '1rem' }}>Patient Profile <br />ID: {id}</Grid>
-        <Grid item xs={12} sx={{ border: '1px solid blue', height: '21rem', marginBottom: '1rem' }}>Sidebar</Grid>
+        <Grid item xs={12} sx={{ marginBottom: '1rem' }}>
+          <Paper elevation={2} sx={{ borderRadius: '8px' }}>
+            <Card variant="outlined" sx={{ borderRadius: '8px', minHeight: '14rem' }}>
+              <CardHeader
+                sx={{ backgroundColor: '#EEE', padding: '0.5rem 1rem' }}
+                avatar={
+                  <Avatar sx={{ backgroundColor: blue[500] }} aria-label="profile-photo" />
+                }
+                action={
+                  <IconButton aria-label="settings">
+                    <MoreVert />
+                  </IconButton>
+                }
+                title="Anshuman Aziz"
+                subheader="Male"
+                titleTypographyProps={{
+                  variant: 'body1'
+                }}
+              />
+              <CardContent>
+                <Grid container>
+                  <Grid item xs={2}>
+                    <Tag />
+                  </Grid>
+                  <Grid item xs={10} sx={{ alignSelf: 'center' }}>
+                    <Typography variant="subtitle2">
+                      MMC-163153
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <PermIdentity />
+                  </Grid>
+                  <Grid item xs={10} sx={{ alignSelf: 'center' }}>
+                    <Typography variant="subtitle2">
+                      {id}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <PhoneAndroid />
+                  </Grid>
+                  <Grid item xs={10} sx={{ alignSelf: 'center' }}>
+                    <Typography variant="subtitle2">
+                      +92 3711659105
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid container sx={{ marginTop: "1rem", justifyContent: "center" }}>
+                  <Button size="small" variant="contained">
+                    Add Reminder
+                  </Button>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sx={{ marginBottom: '1rem' }}>
+          <Paper elevation={2} sx={{ borderRadius: '8px' }}>
+            <Stack>
+              {['Edit Profile', 'Add Invoice', 'Add Token', 'Add File', 'Invoice History'].map((menu) => (
+                <Item>
+                  <ChevronRight sx={{ marginRight: '0.5rem' }} />
+                  <Typography variant="body1">{menu}</Typography>
+                </Item>
+              ))}
+            </Stack>
+          </Paper>
+        </Grid>
         <Grid item xs={12} sx={{ border: '1px solid blue', height: '14rem', marginBottom: '1rem' }}>Patient Follow Up</Grid>
       </Grid>
       <Grid item xs={5}>
